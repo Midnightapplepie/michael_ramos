386d5aa439a9087edfde50f802fa6cb8dd74c168
midnightapplepie <midnightapplepie@gmail.com>
2015-07-31 08:34:27 -0700

    updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
-var http = require('http');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 var request = require('request');
 
 var app = express();
