8e50c329642419826891bfe61a322e47827db4a6
Kevin Xu <kxu@DCO-KXU-M.local>
2015-08-10 20:52:33 -0700

    updated vimeo sample

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var path = require('path');
 var bodyParser = require('body-parser');
 var request = require('request');
@@ -6,10 +7,19 @@ var request = require('request');
 var app = express();
 
 app.set('port',3000);
+
+//setting view engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//setting view directory, where view engine will load templates
+app.set('views',__dirname + '/views');
+
+//app.use will enable specific folder to be accessed by express, enable sendFile/load file from such folder 
 app.use(express.static(path.join(__dirname, 'asset')));
 
 app.get('/',function(request,response){
-	response.sendFile(__dirname + '/views/index.html')
+	response.render("index")
 })
 
 app.listen(app.get('port'), function() {
